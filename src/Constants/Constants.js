f8380f356d2a36c56634eb0a7e21b2c4fc24909b
adharsh2000 <adharshmr001@gmail.com>
2023-05-24 10:17:32 +0530

    first commit

---
+++ src/Constants/Constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = '359e9bf749d10308fc277d99689cd7bf';
+
+export const imageurl = 'https://image.tmdb.org/t/p/original';
